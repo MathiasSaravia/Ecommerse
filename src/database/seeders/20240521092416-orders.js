a322d5c35ebf9957150445d28eeade5829321828
MathiasSaravia <maycolbeach@gmail.com>
2024-07-10 20:35:44 -0300

    fix seed database

--- src/database/seeders/20240521092416-orders.js
+++ src/database/seeders/20240521092416-orders.js
@@ -14,11 +14,11 @@ const orderMappes = orderJSON.map((o) => {
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.bulkInsert('Orders', orderMappes, {});
+    await queryInterface.bulkInsert('orders', orderMappes, {});
   },
 
   async down (queryInterface, Sequelize) {
- await queryInterface.bulkDelete('Orders', null, {});
+ await queryInterface.bulkDelete('orders', null, {});
 
   }
 };
--- src/database/seeders/20240521093710-orderProducts.js
+++ src/database/seeders/20240521093710-orderProducts.js
@@ -22,10 +22,10 @@ console.log(orderProductMapped)
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-  await queryInterface.bulkInsert('OrderProducts', orderProductMapped, {});
+  await queryInterface.bulkInsert('orderProducts', orderProductMapped, {});
   },
 
   async down (queryInterface, Sequelize) {
- await queryInterface.bulkDelete('OrderProducts', null, {});
+ await queryInterface.bulkDelete('orderProducts', null, {});
 }
 };
